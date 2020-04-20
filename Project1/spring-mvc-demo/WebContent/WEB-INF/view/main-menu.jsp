58510540c97dacdac5bc4e802ccfcb31685ec9a2
Nitesh Kumar Tiwari <niteshsoftwaredeveloper@gmail.com>
2020-04-21 01:40:05 +0530

    Spring MVC simple form-validation

--- Project1/spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
+++ Project1/spring-mvc-demo/WebContent/WEB-INF/view/main-menu.jsp
@@ -4,5 +4,10 @@
 <h2>Spring MVC Demo- Home Page</h2>
 <br>
 <a href="showForm">Hello World-Form</a>
+<br><br>
+<a href="student/showForm">Student-Form</a>
+<br><br>
+<a href="customer/showForm">Customer-Form</a>
+<br><br>
 </body>
 </html>
---
+++ Project1/spring-mvc-demo/src/com/nitesh/springdemo/mvc/CustomerController.java
@@ -0,0 +1,34 @@
+package com.nitesh.springdemo.mvc;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@RequestMapping("/showForm")
+	public String showForm(Model theModel) {
+		theModel.addAttribute("customer",new Customer());
+		return "customer-form";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(
+			@Valid @ModelAttribute("customer") Customer theCustomer,
+			BindingResult theBindingResult) {
+		
+		if(theBindingResult.hasErrors()) {
+			return "customer-form";
+		}
+		else {
+			return "customer-confirmation";
+		}
+		
+	}
+}
