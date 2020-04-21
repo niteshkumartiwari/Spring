5af45ea15caf03ee12e2ef5d9dc81b5343d5a345
Nitesh Kumar Tiwari <niteshsoftwaredeveloper@gmail.com>
2020-04-21 16:20:45 +0530

    setting up hibernate:

---
+++ Project1/hibernate-tutorial/src/com/nitesh/hibernate/demo/entity/Student.java
@@ -0,0 +1,27 @@
+package com.nitesh.hibernate.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
+public class Student {
+	@Id
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	public Student() {
+		
+	}
+}
