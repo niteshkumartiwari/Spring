8ed5726586cc1ff870177b00e2e80734b21be43e
Nitesh Kumar Tiwari <niteshsoftwaredeveloper@gmail.com>
2020-04-22 21:07:43 +0530

    AOP: @Before Advice

---
+++ Project1/spring-demo-aop/src/com/nitesh/aopdemo/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.nitesh.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.nitesh.aopdemo")
+public class DemoConfig {
+
+}
