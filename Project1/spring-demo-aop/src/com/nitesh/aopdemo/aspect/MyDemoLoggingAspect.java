2a255caf31cf81e242376a1cd512e1b73db06ac8
Nitesh Kumar Tiwari <niteshsoftwaredeveloper@gmail.com>
2020-04-22 21:27:57 +0530

    AOP: pointcut expressions

--- Project1/spring-demo-aop/src/com/nitesh/aopdemo/aspect/MyDemoLoggingAspect.java
+++ Project1/spring-demo-aop/src/com/nitesh/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-	@Before("execution(public void addAccount())")
+	@Before("execution(public void *Account())")//point-cut expression
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n====>>>> Executing @Before advice on addAccount()");
 	}
