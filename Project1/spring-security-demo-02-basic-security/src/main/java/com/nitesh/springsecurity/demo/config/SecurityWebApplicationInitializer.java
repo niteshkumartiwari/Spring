072f08deb182640acb721478306aa0c3a7389ba4
Nitesh Kumar Tiwari <niteshsoftwaredeveloper@gmail.com>
2020-04-23 23:31:22 +0530

    Basic in-memory login

---
+++ Project1/spring-security-demo-02-basic-security/src/main/java/com/nitesh/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.nitesh.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ Project1/spring-security-demo-02-basic-security/src/main/java/com/nitesh/springsecurity/demo/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.nitesh.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+	@GetMapping("/")
+	public String showHome() {
+		return "home";
+	}
+}
