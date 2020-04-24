e522d52e4d8614f1461e3a934d82b645ee95dec7
Nitesh Kumar Tiwari <niteshsoftwaredeveloper@gmail.com>
2020-04-25 01:13:30 +0530

    spring rest APIs implementations

--- Project1/spring-crm-rest-demo/sql-scripts/01-create-user.sql
+++ Project1/spring-crm-rest-demo/sql-scripts/01-create-user.sql
@@ -1,5 +1,3 @@
-FLUSH PRIVILEGES;
-
 CREATE USER 'springstudent'@'localhost' IDENTIFIED BY 'springstudent';
+
 GRANT ALL PRIVILEGES ON * . * TO 'springstudent'@'localhost';
-ALTER USER 'springstudent'@'localhost' IDENTIFIED WITH mysql_native_password BY 'springstudent';
