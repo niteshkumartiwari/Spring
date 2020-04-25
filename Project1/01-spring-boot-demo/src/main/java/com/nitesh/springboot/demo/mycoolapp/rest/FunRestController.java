663531052e8f5ca0faf5e3bc9fac094f9288a54b
Nitesh Kumar Tiwari <niteshsoftwaredeveloper@gmail.com>
2020-04-25 12:15:20 +0530

    Spring boot demo

---
+++ Project1/01-spring-boot-demo/src/main/java/com/nitesh/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,14 @@
+package com.nitesh.springboot.demo.mycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World! Local time on the server is: "+ LocalDateTime.now();
+	}
+}
